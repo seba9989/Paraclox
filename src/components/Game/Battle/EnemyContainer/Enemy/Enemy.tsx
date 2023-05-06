643959e836fc20f2008d9338ff7445b2a0d06f15
Sebastian Grzybowski <sgrzybbowski@gmail.com>
2023-05-06 16:25:04 +0200

    Enemys SCSS

--- src/components/Game/Battle/EnemyContainer/Enemy/Enemy.tsx
+++ src/components/Game/Battle/EnemyContainer/Enemy/Enemy.tsx
@@ -1,13 +1,24 @@
 import { Enemy } from '../../../../../types/interface'
 import style from './Enemy.module.scss'
 
+function getRandomInt(max: number) {
+	return Math.floor(Math.random() * max)
+}
+
 export default function Enemy(props: { enemy: Enemy }) {
+	let random = getRandomInt(75)
+
 	return (
-		<div className={style.main}>
+		<div className={style.main} style={{ marginBottom: random }}>
+			<div className={style.hpMain}>
+				<div id={`opponentHp-${props.enemy.id}`} className={`${style.hp} opponentHp`}></div>
+				<div>{props.enemy.name}</div>
+			</div>
 			<img
-				src={`https://github.com/seba9989/paraclox/tree/main/src/assets/enemys/${props.enemy.name}/idle.gif?raw=true`}
+				src={`https://github.com/seba9989/paraclox/blob/main/src/assets/enemys/${props.enemy.name}/idle.gif?raw=true`}
 				alt=''
 			/>
 		</div>
 	)
+	random = 0
 }
