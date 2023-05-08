73fc181605332d4332d351c9d4c10e2f2c82cf2a
Sebastian Grzybowski <sgrzybbowski@gmail.com>
2023-05-08 10:40:05 +0200

    rename character-icons

--- src/components/Game/ChoosesCharacter/ChoosesCharacter.tsx
+++ src/components/Game/ChoosesCharacter/ChoosesCharacter.tsx
@@ -1,6 +1,8 @@
 import style from './ChoosesCharacter.module.scss'
 import { useContext } from 'react'
 import GameMachineContext from '../../../context/gameMachineContext'
+import { PlayerBlueprints } from '/src/FakeDB/Tests.json'
+import { Player } from '../../../types/interface'
 
 export default function ChoosesCharacter(props: { select: (name: string) => void }) {
 	const gameMachineContext = useContext(GameMachineContext)
@@ -8,7 +10,27 @@ export default function ChoosesCharacter(props: { select: (name: string) => void
 
 	return (
 		<div className={style.main}>
-			<button onClick={() => gameMachineContext('SELECT_CHARACTER')}>Test</button>
+			<div className={style.characterList}>
+				{PlayerBlueprints.map((character: Player, index: number) => {
+					const { name } = character
+
+					return (
+						<div className={style.radio}>
+							<input type='radio' name='choosesCharacter' id={`${index}`} />
+							<label htmlFor={`${index}`}>
+								<img src='' alt='' />
+							</label>
+						</div>
+					)
+				})}
+			</div>
+			<div className={style.characterIMG}>
+				<img src='https://github.com/seba9989/paraclox/blob/main/src/assets/characters/mage/idle.gif?raw=true' alt='' />
+			</div>
+			<div className={style.characterSkills}></div>
+			<div className={style.characterDescription}></div>
+
+			{/* <button onClick={() => gameMachineContext('SELECT_CHARACTER')}>Test</button> */}
 		</div>
 	)
 }
